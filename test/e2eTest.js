4d483d63d5608faf11f40b09938127b985a46248
Mateusz Buczek <mateusz.buczek@schibsted.pl>
2016-01-25 22:49:54 +0100

    moar API tests

--- test/e2eTest.js
+++ test/e2eTest.js
@@ -23,3 +23,35 @@ describe('POST /stock', function() {
 	});
 });
 
+describe('GET /stock', function() {
+	it('lists all books in stock', function(done) {
+		stockRepository.update("1234", 1);
+		stockRepository.update("5678", 2);
+
+		request(app)
+			.get('/stock')
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.end(function(err, res) {
+				if(err) return done(err);
+				assert.deepEqual(res.body, [{isbn: "1234", count: 1}, {isbn: "5678", count: 2}]);
+				done();
+			});
+	});
+});
+
+describe('GET /stock/:isbn', function() {
+	it('returns number of books by given ISBN', function(done) {
+		stockRepository.update("1234", 1);
+
+		request(app)
+			.get('/stock/1234')
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.end(function(err, res) {
+				if(err) return done(err);
+				assert.deepEqual(res.body, {count: 1});
+				done();
+			});
+	});
+});

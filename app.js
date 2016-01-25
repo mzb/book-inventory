45e93f33d5eb08cf8aae55c49e5a26ca0070f9fc
Mateusz Buczek <mateusz.buczek@schibsted.pl>
2016-01-25 15:08:08 +0100

    MongoDB & promises

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var MongoClient = require('mongodb').MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/book_inventory';
+var db = MongoClient.connect(mongoUrl).then(function(db) {
+	return db.collection('books');
+});
+
 var app = express();
 
 app.use(bodyParser.json());
 
 app.post('/stock', function(req, resp) {
-	resp.send(req.body);
+	var book = req.body;
+	db.then(function(collection) { 
+		collection.updateOne({isbn: book.isbn}, book, {upsert: true}); 
+	})
+	.then(function() { resp.send(book); });
+});
+
+app.get('/stock', function(req, resp) {
+	return db.then(function(collection) { 
+		return collection.find({}).toArray();
+	}).then(function(books) { resp.json(books) });
 });
 
 app.use(function(err, req, resp, next) {
-  resp.status(err.status || 500);
+	resp.status(err.status || 500);
 	resp.json({
 		message: err.message,
 		error: 'production' === process.env.NODE_ENV ? {} : err

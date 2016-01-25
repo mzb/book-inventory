b6449aad6fdbfd11b8aa661d9e4fe2512b2a6344
Mateusz Buczek <mateusz.buczek@schibsted.pl>
2016-01-25 11:22:56 +0100

    express Hello world

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function(req, resp) {
-	resp.end('Hello');
-}).listen(3000, function() {
-	console.log('Server started...');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});

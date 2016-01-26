667676901251217aebaadab78d5e3abe5fcf7579
Mateusz Buczek <mateusz.buczek@schibsted.pl>
2016-01-26 17:25:19 +0100

    Handle content negotiation in /stock/:isbn endpoint.

--- routes.js
+++ routes.js
@@ -18,11 +18,25 @@ module.exports = function(stockRepository) {
 		getCount: function(req, resp, next) {
 			return stockRepository.getCount(req.params.isbn)
 				.then(function(count) {
-					if(count === null) {
-						resp.status(404).json();
-					} else {
-						resp.json({count: count});
-					}
+					resp.format({
+						html: function() {
+							if(count === null) {
+								resp.status(404).send();
+							} else {
+								resp.send(count + "");
+							}
+						},
+						json: function() {
+							if(count === null) {
+								resp.status(404).json();
+							} else {
+								resp.json({count: count});
+							}
+						},
+						'default': function() {
+							resp.sendStatus(415);
+						}
+					});
 				})
 			.catch(next);
 		}
